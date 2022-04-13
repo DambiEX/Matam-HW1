074407b038518b454a01ae15e79c15e23149a607
Dambi <matan.wink@gmail.com>
2022-04-13 17:10:28 +0300

    opened a C project

---
+++ cmake-build-debug/CMakeFiles/Matam_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matam_HW1.dir/main.c.obj"
+  "Matam_HW1.exe"
+  "Matam_HW1.exe.manifest"
+  "Matam_HW1.pdb"
+  "libMatam_HW1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Matam_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
