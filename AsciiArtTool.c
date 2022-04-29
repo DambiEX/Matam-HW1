0e5845f389ea833ee7a4aa82800de5adfb750716
Dambi <matan.wink@gmail.com>
2022-04-29 15:51:28 +0300

    Wrote some terrible functions. see ""//TODO:"s. all new functions need to be inspected closely and potentially bugfixed.

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -1,27 +1,81 @@
 #include "RLEList.c"
 #define READ "r"
+#define WRITE "w"
+#define AT_SIGN '@'
+#define SPACE ' '
 #define NULL_CHAR '\0'
-RLEList asciiArtRead(FILE* in_stream){
-    RLEList list = RLEListCreate();
-    FILE *file = fopen(in_stream, READ);
+
+RLEListResult OpenFile (FILE* source, FILE** destination, char flag){
+    if (!source)
+        return RLE_LIST_NULL_ARGUMENT;
+    FILE *file = fopen(source, flag); //TODO: close the file in case of error? is it needed?
     if (!file) //failed opening the file
+        return RLE_LIST_ERROR; //TODO: return the right errors
+    *destination = file;
+    return RLE_LIST_SUCCESS;
+}
+
+RLEListResult InvertNode(Node node){
+    if (!node)
+        return RLE_LIST_NULL_ARGUMENT;
+    else if (node->symbol == AT_SIGN)
+        node->symbol = SPACE;
+    else if (node->symbol == SPACE)
+        node->symbol = AT_SIGN;
+    return RLE_LIST_SUCCESS;
+}
+
+RLEListResult InvertList (RLEList list, char replacing, char replaced){
+    if (!list || !replacing || !replaced){
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEListMap(list, InvertChar)
+}
+
+RLEList asciiArtRead(FILE* in_stream){
+    FILE *file;
+    RLEListResult result = OpenFile(in_stream, file, READ);
+    if (result != RLE_LIST_SUCCESS)
         return NULL;
+
+    RLEList list = RLEListCreate();
     char content = NULL_CHAR;
     bool scan_success = true;
     while (scan_success)
     {
-        scan_success = fgets(content, 1, file); //fscanf(file, &content); //TODO: fix syntax
+        scan_success = fgets(&content, 1, file); //fscanf(file, &content); //TODO: fix syntax
         RLEListAppend(list, content);
     }
 
-    if (RLEListSize(list) == 0)
-        return NULL; //received an empty file
-    fclose(in_stream);
+    if (RLEListSize(list) == 0) //received an empty file
+        return NULL;
+    fclose(file);
     return list;
 }
+
 RLEListResult asciiArtPrint(RLEList list, FILE *out_stream){
+    FILE *file;
+    RLEListResult result = OpenFile(out_stream, file, WRITE);
+    if (result != RLE_LIST_SUCCESS)
+        return result;
 
+    Node node = list->first_node;
+    while (node->next){
+        for (int i = 0; i < node->next->repetitions; ++i) {
+            fprintf(file, node->next->symbol); //TODO: nicer syntax to write all the repitions at once?
+        }
+    }
+    fclose(file);
+    return RLE_LIST_SUCCESS;
 }
-RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream){
 
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream){
+    FILE *file;
+    RLEListResult result = OpenFile(out_stream, file, WRITE);
+    if (result != RLE_LIST_SUCCESS)
+        return result;
+
+    RLEListResult print_result;
+    fprintf(RLEListExportToString(list, print_result), file);
+    return print_result;
 }
--- main.c
+++ main.c
@@ -1,5 +1,53 @@
-#include <stdlib.h>
+#include "AsciiArtTool.c"
+#define ENCODED 'e'
+#define INVERTED 'i'
+#define FLAGGED '-'
+typedef char FLAG;
 
-int main(){
-    return 0;
+RLEListResult ParseFlag (FLAG* destination, FLAG* source){
+    if (source[0] == FLAGGED)
+    {
+        if (source[1] == ENCODED || source[1] == INVERTED)
+        {
+            *destination = source[1];
+            return RLE_LIST_SUCCESS;
+        }
+        else
+            return RLE_LIST_ERROR;
+    }
+    else if (source[0]) // != NULL and != FLAGGED
+        return RLE_LIST_ERROR;
+    return RLE_LIST_ERROR; //TODO: return error if (source[0] == NULL)?
+}
+
+RLEListResult UnpackInput(int argc, char **argv,
+                          FLAG* flag, FILE *source, FILE *destination){
+    if (argc != 4) // 3 args + 1 default
+        return RLE_LIST_NULL_ARGUMENT; // invalid amount of args
+    RLEListResult valid_input = ParseFlag(flag, argv[1]);
+    if (valid_input != RLE_LIST_SUCCESS)
+        return valid_input;
+    valid_input = OpenFile(argv[2], source, READ);
+    if (valid_input != RLE_LIST_SUCCESS)
+        return valid_input;
+    valid_input = OpenFile(argv[3], destination, WRITE);
+    return valid_input;
+}
+
+int main(int argc, char** argv){
+    FLAG flag;
+    FILE *source, *destination; //TODO: make sure pointer syntax is correct.
+    RLEListResult unpacked_success = UnpackInput(argc, argv, &flag, source, destination);
+    if (unpacked_success != RLE_LIST_SUCCESS)
+        return NULL;
+
+    RLEList list = asciiArtRead(source);
+
+    if (flag == ENCODED){
+        asciiArtPrintEncoded(list, destination);
+    }
+    else if (flag == INVERTED){
+        RLEListMap(list, InvertNode);
+    }
+
 }
